7fb56d663a4658f71d07d21f1459dacca848a29b
pedro <pedro_moreira7@hotmail.com>
2018-08-13 14:20:28 +0100

    Astrology_tabs astrology pages created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mystic_app/astrology/astrology_header.dart';
-import 'package:mystic_app/we_mystic_news.dart';
+import 'package:mystic_app/bottom_nav_bar.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,61 +13,4 @@ class MyApp extends StatelessWidget{
     );
   }
 }
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-
-class _HomePageState extends State<HomePage> {
-
-  int currentTab = 0;
-  WeMysticNewsData home;
-  AstrologyTabs astrology;
-  List<Widget> pages;
-  Widget currentPage;
-  @override
-  void initState(){
-    astrology = AstrologyTabs();
-    home = WeMysticNewsData();
-
-    pages = [home, astrology];
-
-    currentPage = home;
-
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: currentPage,
-        bottomNavigationBar: new BottomNavigationBar(
-          currentIndex: currentTab,
-            onTap: (int index){
-              setState(() {
-                currentTab = index;
-                currentPage = pages[index];
-              });
-            },
-            items: <BottomNavigationBarItem>[
-              new BottomNavigationBarItem(
-                icon: const Icon(
-                  Icons.home,
-                ),
-                title: new Text('Home'),
-              ),
-
-              new BottomNavigationBarItem(
-                icon: const Icon(Icons.home),
-                title: new Text('Astrology'),
-              ),
-            ]));
-  }
-
-}
-
-
-
 
